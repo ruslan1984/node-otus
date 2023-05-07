5fcb822a88cfdfced66b9b39d85927a55305cccb
Ruslan <ruslan231984@yandex.ru>
2023-05-07 17:17:54 +0300

    remove old

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,9 @@
 import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AuthModule } from "./api/auth/auth.module";
-import { UsersModule } from "./api/users/users.module";
-import { PostsResolver } from "./api/posts/posts.resolver";
-import { GraphQLModule } from "@nestjs/graphql";
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-import { PostsService } from "src/api/posts/posts.service";
-import { PostEntity } from "src/api/posts/post.entity";
 import { AppController } from "./app.controller";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "",
-      password: "",
-      database: "otus",
-      entities: [PostEntity],
-      synchronize: false,
-    }),
-
-    TypeOrmModule.forFeature([PostEntity]),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      installSubscriptionHandlers: true,
-      autoSchemaFile: true,
-    }),
-
-    AuthModule,
-    UsersModule,
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [PostsResolver, PostsService],
+  providers: [],
 })
 export class AppModule {}
