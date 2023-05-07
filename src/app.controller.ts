ba35ab70fb11e933195daadfee5f6a7380b5b6f1
Ruslan <ruslan231984@yandex.ru>
2023-05-08 00:48:52 +0300

    lint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get, Request } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get("/")
-  getProfile() {
-    return "Hello!";
+  @Get('/')
+  sayHello() {
+    return 'Hello!';
   }
 }
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,5 @@
-describe("test", () => {
-  it("test0", () => {
+describe('test', () => {
+  it('test0', () => {
     expect(1).toEqual(1);
   });
 });
