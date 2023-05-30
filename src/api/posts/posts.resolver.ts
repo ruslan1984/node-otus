16f3ad110a6d942ea6456f18857432ab7156b5c4
Ruslan <ruslan231984@yandex.ru>
2023-05-30 13:06:30 +0300

    auth

--- src/api/posts/posts.resolver.ts
+++ src/api/posts/posts.resolver.ts
@@ -3,14 +3,14 @@ import { UseGuards } from "@nestjs/common";
 import { PostEntity, PostListEntity } from "./post.entity";
 import { PostsService } from "./posts.service";
 import { UpdatePostInput, CreatePostInput, PaginateInput } from "./args";
+import { JwtAuthGuard } from "src/api/auth/jwt-auth.guard";
 
 @Resolver((of) => PostEntity)
 export class PostsResolver {
   constructor(private readonly postsService: PostsService) {}
-  @Query(() => PostListEntity, { name: "posts" }) 
-  async getPosts(
-    @Args("paginateInput") paginateInput: PaginateInput
-  ) {
+
+  @Query(() => PostListEntity, { name: "posts" })
+  async getPosts(@Args("paginateInput") paginateInput: PaginateInput) {
     return this.postsService.getPosts(paginateInput);
   }
 
@@ -19,6 +19,7 @@ export class PostsResolver {
     return this.postsService.getPost(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => PostEntity, { name: "updatePost" })
   updatePost(
     @Args("id") id: number,
@@ -27,11 +28,13 @@ export class PostsResolver {
     return this.postsService.update(id, updatePostInput);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => PostEntity, { name: "createPost" })
   createPost(@Args("createPostInput") createPostInput: CreatePostInput) {
     return this.postsService.create(createPostInput);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => PostEntity, { name: "deletePost" })
   deletePost(@Args("id") id: number) {
     return this.postsService.delete(id);
